5b50358170659d01efb9001ef9946be8ac8b968b
Danny Yoo <dyoo@hashcollision.org>
2013-03-12 21:28:49 -0700

    figuring out how to use the lru code in the untyped world.

--- untyped-client.rkt
+++ untyped-client.rkt
@@ -1,10 +1,33 @@
-#lang racket
-;; This file does not yet compile.  Asking on the list why.
+#lang racket/base
 
-(require "lru.rkt")
+;; Using the LRU in untyped code is a little awkward.  We can
+;; instantiate the module for specific types:
+(module string-lru typed/racket/base
+  (require (prefix-in poly: "lru.rkt"))
+  (provide make-lru lru-ref lru-set! lru-has-key?)
+  
+  (: make-lru (Natural -> (poly:Lru String String)))
+  (define make-lru poly:make-lru)
+  
+  (: lru-ref ((poly:Lru String String) String -> (U String #f)))
+  (define (lru-ref a-lru key)
+    (poly:lru-ref a-lru key (lambda () #f)))
+  
+  (: lru-set! ((poly:Lru String String) String String -> Void))
+  (define lru-set! poly:lru-set!)
+  
+  (: lru-has-key? ((poly:Lru String String) String -> Boolean))
+  (define lru-has-key? poly:lru-has-key?))
 
-(define l (make-lru 5))
+
+;; after which we can use the library.
+(require (submod "." string-lru))
+(define l (make-lru 2))
 (lru-set! l "greeting" "hello")
-
-;; The following call here fails to typecheck?
+(lru-set! l "program" "lru test")
+(lru-set! l "language" "racket")
 (lru-ref l "greeting")
+(lru-ref l "program")
+(lru-ref l "language")
+
+
